047987efea8df2f2564de462d49af824db3b647e
robin-whg <mail@robin-whg.com>
2022-02-08 16:28:36 +0100

    build: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.green,
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
