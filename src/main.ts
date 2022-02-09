87134f735d561d3a3b66150c10c62d53a2a9091e
robin-whg <mail@robin-whg.com>
2022-02-09 10:43:06 +0100

    build: add vueuse and pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles/index.css";
 
-import { createRouter } from "./modules";
+import { createRouter, createHead, createPinia } from "./modules";
 
-createApp(App).use(createRouter()).mount("#app");
+createApp(App)
+  .use(createRouter())
+  .use(createHead())
+  .use(createPinia())
+  .mount("#app");
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,1 +1,3 @@
 export * from "./router";
+export * from "./head";
+export * from "./pinia";
