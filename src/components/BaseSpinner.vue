20cb14efd46911b0e1c023fa33e99aabd116e601
robin-whg <mail@robin-whg.com>
2022-03-03 10:32:11 +0100

    fix: spinner component styling

--- src/components/BaseSpinner.vue
+++ src/components/BaseSpinner.vue
@@ -8,3 +8,14 @@
     <circle cx="50" cy="50" r="45" />
   </svg>
 </template>
+
+<style lang="postcss" scoped>
+circle {
+  fill: transparent;
+  stroke: currentColor;
+  stroke-width: 9;
+  stroke-dasharray: 214;
+  stroke-linecap: round;
+  transform-origin: 50% 50%;
+}
+</style>
