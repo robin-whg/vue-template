72c0062e811188bd336e205aba27a4f7a9710146
robin-whg <mail@robin-whg.com>
2022-03-03 10:31:42 +0100

    feat: single primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
       colors: {
-        primary: colors.green,
+        primary: "#42b883",
       },
     },
   },
