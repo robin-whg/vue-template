3ee27e2f54b7e30506cc8b3695b726e377de7b61
robin-whg <mail@robin-whg.com>
2022-03-11 14:04:45 +0100

    feat: base listbox component

--- src/components/BaseListbox.vue
+++ src/components/BaseListbox.vue
@@ -5,61 +5,74 @@ interface Props {
   label: string;
   srOnly?: boolean;
   error?: string | Ref<string | undefined>;
+  modelValue: string;
+  options: string[];
 }
 const props = withDefaults(defineProps<Props>(), {
   srOnly: false,
   error: "",
 });
-const { label, srOnly } = toRefs(props);
+const { modelValue, options, label, srOnly } = toRefs(props);
 
-const people = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
-];
-const selectedPerson = ref(people[0]);
+const emit = defineEmits<{
+  (event: "update:modelValue", value: string): void;
+}>();
+
+const localModelValue = ref(modelValue.value);
+watch(localModelValue, (value) => {
+  emit("update:modelValue", localModelValue.value);
+});
 </script>
 
 <template>
-  <listbox v-model="selectedPerson" as="div" class="flex flex-col gap-1">
-    <listbox-label
+  <h-listbox v-model="localModelValue" as="div" class="flex flex-col gap-1">
+    <h-listbox-label
       class="relative text-sm font-medium text-zinc-700 dark:text-zinc-200"
       :class="{ 'sr-only': srOnly }"
-      >{{ label }}</listbox-label
+      >{{ label }}</h-listbox-label
     >
     <div class="relative">
-      <listbox-button
-        class="relative inline-flex w-full items-center justify-between rounded-lg border px-4 py-2 text-sm font-medium text-zinc-700 transition duration-100 ease-in-out hover:bg-zinc-100 disabled:opacity-75 dark:text-zinc-200 dark:hover:bg-zinc-800"
+      <h-listbox-button
+        class="relative inline-flex w-full items-center justify-between rounded-lg border bg-white px-4 py-2 text-sm font-medium text-zinc-700 transition duration-100 ease-in-out hover:bg-zinc-100 focus:border-transparent focus:outline-none focus:ring disabled:opacity-50 motion-reduce:transition-none dark:bg-zinc-900 dark:text-zinc-200 dark:hover:bg-zinc-800 dark:focus:border-transparent"
         :class="[
-          error ? 'border-red-500' : 'border-zinc-300 dark:border-zinc-600',
+          error
+            ? 'border-red-500   focus:ring-red-500/50'
+            : 'border-zinc-300  focus:ring-primary/50 dark:border-zinc-600',
         ]"
-        >{{ selectedPerson.name }}
+        >{{ modelValue }}
         <i-heroicons-solid-selector
           class="h-5 w-5 text-zinc-400 dark:text-zinc-500"
         />
-      </listbox-button>
-      <listbox-options
-        class="absolute mt-1 w-full divide-y divide-zinc-300 overflow-hidden rounded-lg border border-zinc-300 bg-white dark:divide-zinc-600 dark:border-zinc-600 dark:bg-zinc-900"
+      </h-listbox-button>
+      <h-listbox-options
+        class="absolute mt-1 w-full divide-y overflow-hidden rounded-lg border border-zinc-300 bg-white focus:outline-none dark:border-zinc-600 dark:bg-zinc-900"
+        :class="[
+          error
+            ? 'divide-red-500 border-red-500'
+            : 'divide-zinc-300   border-zinc-300 dark:divide-zinc-600 dark:border-zinc-600',
+        ]"
       >
-        <listbox-option
-          v-for="person in people"
-          v-slot="{ selected }"
-          :key="person.id"
-          :value="person"
-          :disabled="person.unavailable"
-          class="flex cursor-pointer items-center gap-2 px-4 py-2 text-sm font-medium text-zinc-700 transition duration-100 ease-in-out hover:bg-zinc-100 dark:text-zinc-200 dark:hover:bg-zinc-800"
+        <h-listbox-option
+          v-for="option in options"
+          v-slot="{ selected, active }"
+          :key="option"
+          :value="option"
+          as="template"
         >
-          <i-heroicons-solid-check
-            :class="selected ? '' : 'invisible'"
-            class="h-5 w-5 text-primary"
-          />
-          <span class="truncate">
-            {{ person.name }}
-          </span>
-        </listbox-option>
-      </listbox-options>
+          <div
+            class="flex cursor-pointer items-center gap-2 px-4 py-2 text-sm font-medium text-zinc-700 transition duration-100 ease-in-out hover:bg-zinc-100 motion-reduce:transition-none dark:text-zinc-200 dark:hover:bg-zinc-800"
+            :class="{ 'bg-zinc-100 dark:bg-zinc-800': active }"
+          >
+            <i-heroicons-solid-check
+              :class="{ invisible: !selected }"
+              class="h-5 w-5 text-primary"
+            />
+            <span class="truncate">
+              {{ option }}
+            </span>
+          </div>
+        </h-listbox-option>
+      </h-listbox-options>
     </div>
     <span
       v-if="error"
@@ -68,5 +81,5 @@ const selectedPerson = ref(people[0]);
       <i-heroicons-solid-exclamation-circle class="h-4 w-4" />
       {{ error }}
     </span>
-  </listbox>
+  </h-listbox>
 </template>
