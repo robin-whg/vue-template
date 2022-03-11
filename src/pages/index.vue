1f566cf098fd0e264c71b8f308d3a083b6647c81
robin-whg <mail@robin-whg.com>
2022-03-11 14:44:24 +0100

    chore: examples

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
+import { toggleDark } from "~/composables";
+
 const textInput = ref("");
+
 const switchValue = ref(false);
+
+const options = ["Option 1", "Option 2", "Option 3"];
+const currentOption = ref<string>(options[0]);
 </script>
 
 <template>
@@ -12,29 +16,26 @@ const switchValue = ref(false);
       Button</base-button
     >
     <base-button variant="primary" loading>Button</base-button>
-    <base-button variant="secondary" icon>
-      <i-heroicons-outline-home class="h-6 w-6" />
+    <base-button variant="secondary" icon @click="toggleDark()">
+      <i-heroicons-outline-moon class="h-6 w-6" />
     </base-button>
-    <base-button variant="secondary">Button</base-button>
-    <base-button variant="tertiary" icon @click="toggleDark()">
+    <base-button variant="tertiary" icon>
       <i-heroicons-outline-cloud-upload class="h-6 w-6" />
       Upload
     </base-button>
   </div>
   <div class="mx-auto flex max-w-xl flex-col gap-4">
-    <!-- <base-input
+    <base-button variant="secondary">Button</base-button>
+    <base-input
       v-model="textInput"
-      label="Text Input"
+      label="This is a text input"
       placeholder="placeholder"
-      error="Requried"
     />
-    <base-radio-group label="Radio Group" error="Required">
-      <base-radio-group-option value="test"> Startup </base-radio-group-option>
-      <base-radio-group-option value="test2"> Startup </base-radio-group-option>
-    </base-radio-group>
-    <base-listbox label="Listbox" error="Required" /> -->
-    <base-switch v-model="switchValue" label="This is a switch." />
-    <p>{{ switchValue }}</p>
-    <!-- <base-checkbox-group /> -->
+    <base-switch v-model="switchValue" label="This is a switch" />
+    <base-listbox
+      v-model="currentOption"
+      label="This is a listbox"
+      :options="options"
+    />
   </div>
 </template>
