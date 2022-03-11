91aea4242267f26dca902d7bd06f5c3f1d7ae805
robin-whg <mail@robin-whg.com>
2022-03-11 14:42:02 +0100

    feat: style like form components

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -30,7 +30,7 @@ const variantClass = computed(() => getVariantClass());
 <template>
   <component
     :is="as"
-    class="inline-flex items-center justify-center rounded-lg border text-sm font-medium transition duration-100 ease-in-out hover:shadow-md disabled:opacity-75"
+    class="inline-flex items-center justify-center rounded-lg border text-sm font-medium transition duration-100 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-primary/50 disabled:opacity-50 motion-reduce:transition-none"
     :class="[icon ? 'p-1.5' : 'px-4 py-2', variantClass]"
     :disabled="loading"
   >
@@ -46,14 +46,14 @@ const variantClass = computed(() => getVariantClass());
 
 <style lang="postcss" scoped>
 .btn-primary {
-  @apply border-transparent bg-primary text-white hover:shadow-primary/50 dark:text-gray-900;
+  @apply border-transparent bg-primary text-white hover:shadow-md hover:shadow-primary/50 dark:text-zinc-900;
 }
 
 .btn-secondary {
-  @apply border-gray-300 text-gray-700 hover:bg-gray-400/20 hover:shadow-gray-400/20 dark:border-gray-600 dark:text-gray-200 dark:hover:bg-gray-500/20 dark:hover:shadow-gray-500/20;
+  @apply border-zinc-300 text-zinc-700 hover:bg-zinc-100 dark:border-zinc-600 dark:text-zinc-200 dark:hover:bg-zinc-800;
 }
 
 .btn-tertiary {
-  @apply border-transparent text-gray-700 hover:bg-gray-400/20  hover:shadow-gray-400/20 dark:text-gray-200 dark:hover:bg-gray-500/20 dark:hover:shadow-gray-500/20;
+  @apply border-transparent text-zinc-700 hover:bg-zinc-100 dark:text-zinc-200 dark:hover:bg-zinc-800;
 }
 </style>
