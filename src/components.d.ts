a6eb79ab0f455526b0c432565a820d267332f86b
robin-whg <mail@robin-whg.com>
2022-05-23 15:17:09 +0200

    update

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,20 +5,22 @@
 declare module 'vue' {
   export interface GlobalComponents {
     BaseButton: typeof import('./components/BaseButton.vue')['default']
-    BaseCheckboxGroup: typeof import('./components/BaseCheckboxGroup.vue')['default']
     BaseInput: typeof import('./components/BaseInput.vue')['default']
     BaseListbox: typeof import('./components/BaseListbox.vue')['default']
-    BaseRadioGroup: typeof import('./components/BaseRadioGroup.vue')['default']
-    BaseRadioGroupOption: typeof import('./components/BaseRadioGroupOption.vue')['default']
     BaseSpinner: typeof import('./components/BaseSpinner.vue')['default']
     BaseSwitch: typeof import('./components/BaseSwitch.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
+    HListbox: typeof import('@headlessui/vue')['Listbox']
+    HListboxButton: typeof import('@headlessui/vue')['ListboxButton']
+    HListboxLabel: typeof import('@headlessui/vue')['ListboxLabel']
+    HListboxOption: typeof import('@headlessui/vue')['ListboxOption']
+    HListboxOptions: typeof import('@headlessui/vue')['ListboxOptions']
     HSwitch: typeof import('@headlessui/vue')['Switch']
-    HSwitchGroup: typeof import('@headlessui/vue')['SwitchGroup']
-    HSwitchLabel: typeof import('@headlessui/vue')['SwitchLabel']
     IHeroiconsOutlineCloudUpload: typeof import('~icons/heroicons-outline/cloud-upload')['default']
-    IHeroiconsOutlineHome: typeof import('~icons/heroicons-outline/home')['default']
+    IHeroiconsOutlineMoon: typeof import('~icons/heroicons-outline/moon')['default']
     IHeroiconsSolidBeaker: typeof import('~icons/heroicons-solid/beaker')['default']
+    IHeroiconsSolidCheck: typeof import('~icons/heroicons-solid/check')['default']
+    IHeroiconsSolidExclamationCircle: typeof import('~icons/heroicons-solid/exclamation-circle')['default']
+    IHeroiconsSolidSelector: typeof import('~icons/heroicons-solid/selector')['default']
   }
 }
 
